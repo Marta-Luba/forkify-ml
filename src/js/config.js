af02b7ea609bb7dcf03d2140315f9c7a55914567
Marta-Luba <marta.luba@yahoo.co.uk>
2023-09-27 10:51:20 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2b42b362-2bc1-4f25-b61d-bc125d5523bb';
+export const MODAL_CLOSE_SEC = 2.5;
